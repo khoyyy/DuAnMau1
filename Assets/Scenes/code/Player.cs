03f64e31b713ae4fc3d7b357bcf86684293f5a84
khoyyy <nguyenminhkhoi100606@gmail.com>
2024-05-31 15:01:47 +0700

    dấd

--- Assets/Scenes/code/Player.cs
+++ Assets/Scenes/code/Player.cs
@@ -1,19 +1,31 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 5f;
+    private Rigidbody2D _rigidbody2D;
+    Vector2 moveInput;
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+    void onMove (InputValue value)  
+    {
+        moveInput  = value.Get<Vector2>();
+    }
+    // chuyển động của nhân vật 
+    void Run()
+    {
+        Vector2 moveVelocity = new Vector2(moveInput.x * moveSpeed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector2(moveInput.x, y:0f);
     }
 }
