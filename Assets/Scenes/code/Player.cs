90efed5dabe9dcbcf093fd53c41d2145a43440a0
khoyyy <nguyenminhkhoi100606@gmail.com>
2024-06-01 13:08:00 +0700

    vvv

--- Assets/Scenes/code/Player.cs
+++ Assets/Scenes/code/Player.cs
@@ -5,44 +5,95 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
-    private Rigidbody2D _rigidbody2D;
-    Vector2 moveInput;
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth = 10;
+    [SerializeField] private float movespeed;
+    [SerializeField] private float jumpspeed;
+    [SerializeField] private float climSpeed = 5f;
+    private bool isclimp;
+    Rigidbody2D rb;
+    Animator animator;
+    public Transform _canjump;
+    public LayerMask nen;
+    private bool canjump;
+    //private bool doublejump;
+    private bool _flip;
+    [SerializeField] private float down;
+    Vector2 Vector;
+    Vector2 up;
+    public GameObject bulletPrefab;
+    public Transform guntransform;
+    CapsuleCollider2D capsuleCollider;
+    private float gravti;
+
+    
+
+
     void Start()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        Vector = new Vector2(0, -Physics2D.gravity.y);
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        gravti = rb.gravityScale;
+        health = maxHealth;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Run();
-        FlipSprite();
+        Move();
+        
+        //ClimpLadder();
     }
-    void onMove (InputValue value)  
+    private void Move()
     {
-        moveInput  = value.Get<Vector2>();
-        Debug.Log(message: "......... Move Input:" + moveInput);
-       
-    }
-    // chuyển động của nhân vật 
-    void Run()
-    {
-        var moveVelocity = new Vector2(moveInput.x * moveSpeed, _rigidbody2D.velocity.y);
-        _rigidbody2D.velocity = moveVelocity;
+        canjump = Physics2D.OverlapCircle(_canjump.position, 0.2f, nen);
+        var Move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(Move * movespeed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.W) && canjump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+            animator.SetBool("2Jump", true);
+            animator.SetBool("ak", false);
+        }
+        if (Move > 0)
+        {
+            _flip = true;
+        }
+        else if (Move < 0)
+        {
+            _flip = false;
+        }
+        if (rb.velocity.y == 0 && Move > 0 || rb.velocity.y == 0 && Move < 0)
+        {
+            animator.SetBool("isRun", true); animator.SetBool("ak", false);
+        }
+        if (Move == 0 && rb.velocity.y == 0)
+        {
+            animator.SetBool("isRun", false);
+
+            animator.SetBool("2Jump", false);
+        }
+        if (rb.velocity.y == 0)
+        {
+            animator.SetBool("2Jump", false);
+            animator.SetBool("falling", false);
+
+        }
+        if (rb.velocity.y < 0)
+        {
+            animator.SetBool("falling", true);
+
+            rb.velocity -= Vector * down * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            animator.SetBool("ak", true);
+        }
+        else animator.SetBool("ak", false);
+        transform.localScale = _flip ? new Vector2(1.510177f, 1.169045f) : new Vector2(-1.510177f, 1.169045f);
     }
 
-    // abs: giá trị tuyệt đối 
-    // sign : đấu của giá trị
-    // epsilon: giá trị nhỏ nhất có thể so sánh
-    // xoay hướng nhân vật theo chiều chuyển động 
-    void FlipSprite()
-    {
-        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed)
-        {
-            transform.localScale = new Vector2(x:Mathf.Sign(_rigidbody2D.velocity.x), y:1f);
-        }
-    }
-    
 }
